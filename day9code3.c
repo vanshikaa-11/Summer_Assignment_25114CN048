161eb90fd3a8d63dcc55d52120d910f72ddba518
vanshikaa-11 <vanshikamalik52@gmail.com>
2026-06-09 16:48:18 +0530

    day 9

---
+++ day9code3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    for (int i=65;i<=69;i++){
+        for (int j=65;j<=i;j++){
+            printf("%c",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
