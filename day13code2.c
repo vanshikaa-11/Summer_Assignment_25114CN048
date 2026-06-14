19d832b4837a942a36b43a2c7b61a83fc9fd25b8
vanshikaa-11 <vanshikamalik52@gmail.com>
2026-06-14 14:26:50 +0530

    day 13

---
+++ day13code2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int arr[100],n,i,sum=0;
+    float avg;
+    printf("Enter numbers of elements : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }
+    avg=(float)sum/n;
+    printf("Sum = %d\n",sum);
+    printf("Average=%.2f\n",avg);
+    return 0;
+}
